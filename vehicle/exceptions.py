3188f7165dff9980c80b5b4af328216bff40a2da
NatalyaOshmarina <91863446+NatalyaOshmarina@users.noreply.github.com>
2024-03-14 10:43:32 +0500

    created project

---
+++ vehicle/exceptions.py
@@ -0,0 +1,10 @@
+def LowFuelError():
+    print('Залейте бензин')
+
+
+def NotEnoughFuel(fuel):
+    print('Необходимо дозаправить {} литров'.format(fuel))
+
+
+def CargoOverload(weight):
+    print('Перегруз {} килограмм'.format(weight))
